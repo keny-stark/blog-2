377cbd3f2354f4fdd2979ed9e492aedad2d646cb
keny-stark <abdurakhmanov_i@iuca.kg>
2019-09-30 10:39:46 +0600

    добавил в темплуте html

--- source/main/urls.py
+++ source/main/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='index'),
+    path('article/<int:pk>/', ArticleView.as_view(), name='article_view'),
+    path('article/add/', ArticleCreateView.as_view(), name='article_add'),
+    path('article/<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_update'),
+    path('article/<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
+    path('comment/add/', CommentCreateView.as_view(), name='comment_add')
 ]
